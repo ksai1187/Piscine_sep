b3b565f4f9d247a378a539125dde537aa175a839
ksaitou <ksaitou@ksaitounoMacBook-Air.local>
2025-09-08 11:24:00 +0900

    fixed

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -39,12 +39,10 @@ char *ft_strcapitalize(char *str)
 #include <stdio.h>
 int main(void)
 {
-    // テストケース 1
     char test1[] = "hi, how are you? 42words forty-two; fifty+and+one";
     printf("before: %s\n", test1);
     printf("after : %s\n\n", ft_strcapitalize(test1));
 
-    // テストケース 2
     char test2[] = "HELLO wOrLd! 123abc TEST-case";
     printf("before: %s\n", test2);
     printf("after : %s\n\n", ft_strcapitalize(test2));
