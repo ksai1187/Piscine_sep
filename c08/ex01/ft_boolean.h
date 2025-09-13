d58c94960ebc6be1b857475348fd1d7da7da8834
ksaitou <ksaitou@ksaitounoMacBook-Air.local>
2025-09-14 06:36:21 +0900

    aaa

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,19 @@
+#ifndef BOOL_H
+#define BOOL_H
+
+typedef enum e_bool
+{
+	FALSE = 0,
+	TRUE = 1
+}t_bool;
+
+#define EVEN(nbr) ((nbr) % 2 == 0)
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG "I have an odd number of arguments."
+#define SUCCESS 0
+void ft_putstr(char *str);
+
+t_bool ft_is_even(int nbr);
+
+
+#endif
---
+++ c08/ex01/ft_is_even.c
@@ -0,0 +1,5 @@
+#include "ft_boolean.h"
+t_bool  ft_is_even(int nbr)
+{
+    return ((nbr % 2) == 0 ? TRUE : FALSE);
+}
